fac423b5148af25a956fff9b5380495ecc15b123
akashmorey18 <107257260+akashmorey18@users.noreply.github.com>
2023-08-28 15:50:32 +0530

    temp.py

--- temp.py
+++ temp.py
@@ -8,11 +8,15 @@ from openpyxl import load_workbook
 
 import streamlit as st
 from PIL import Image
-import office365
+# import office365
 
 # from office365.runtime.auth.authentication_context import AuthenticationContext
 # from office365.sharepoint.client_context import ClientContext
 # from office365.sharepoint.files.file_creation_information import FileCreationInformation
+
+from office365.sharepoint.client_context import ClientContext
+from office365.runtime.auth.authentication_context import AuthenticationContext
+from office365.sharepoint.files.file import File
 
 # SharePoint authentication using client ID and client secret
 
