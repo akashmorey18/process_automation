da94383a1285bd6778154c3c02a1ef46d2e7a32c
akashmorey18 <107257260+akashmorey18@users.noreply.github.com>
2023-08-28 18:25:23 +0530

    temp.py

--- temp.py
+++ temp.py
@@ -25,12 +25,15 @@ ctx = ClientContext(site_url, ctx_auth)
 
 # Upload a file to SharePoint using the office365 library
 def upload_to_sharepoint(file_content, file_name, folder_url):
-    file_info = FileCreationInformation()
-    file_info.url = file_name
+    ctx_auth = AuthenticationContext(url=site_url)
+    ctx_auth.acquire_token_for_app(client_id, client_secret)
+    ctx = ClientContext(site_url, ctx_auth)
     
-    # Upload the file content using ContentStream
-    ctx.web.get_folder_by_server_relative_url(folder_url).files.add_using_path(
-        file_info, file_content, overwrite=True)
+    target_folder = ctx.web.get_folder_by_server_relative_url(folder_url)
+    file_info = FileCreationInformation()
+    file_info.content = file_content
+    file_info.url = file_name
+    uploaded_file = target_folder.files.add(file_info)
     ctx.execute_query()
     
 # Set the page title and header
