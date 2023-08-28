2f00ce1659dd1870bb9db722c5135d3fd313b3f5
akashmorey18 <107257260+akashmorey18@users.noreply.github.com>
2023-08-28 15:02:36 +0530

    temp.py

--- temp.py
+++ temp.py
@@ -6,14 +6,18 @@ import streamlit as st
 from PIL import Image
 from openpyxl import load_workbook
 
+import streamlit as st
+from PIL import Image
 from office365.runtime.auth.authentication_context import AuthenticationContext
 from office365.sharepoint.client_context import ClientContext
-from office365.sharepoint.file_creation_information import FileCreationInformation
+from office365.sharepoint.files.file_creation_information import FileCreationInformation
 
 # SharePoint authentication using client ID and client secret
-site_url = "/sites/AutomationProject/Shared%20Documents/BB/"
+
+site_url = "https://tatadigitalltd.sharepoint.com/sites/AutomationProject"
 client_id = '96026163-d873-4cca-abd4-7acf4fa3e1a3'
 client_secret = 'DolnVP4E9IIoLNf/86GHbABu+djbFE679RerpV5Z9Hs='
+
 
 ctx_auth = AuthenticationContext(url=site_url)
 ctx_auth.acquire_token_for_app(client_id, client_secret)
@@ -26,7 +30,6 @@ def upload_to_sharepoint(file_content, file_name, folder_url):
     target_folder = ctx.web.get_folder_by_server_relative_url(folder_url)
     uploaded_file = target_folder.files.add(file_info)
     ctx.execute_query()
-
 
 
 # Set the page title and header
@@ -119,9 +122,9 @@ if selected_page == "Creatives Upload" :
 
         if st.button("Upload to SharePoint"):
             image_content = uploaded_image.read()
-        #     folder_url = "/sites/your-site/Shared Documents/FolderName"
-        #     upload_to_sharepoint(image_content, uploaded_image.name, folder_url)
-        #     st.success("Image uploaded to SharePoint!")
-    
+            folder_url = "/sites/AutomationProject/Shared Documents/Fashion Merchandising"
+            upload_to_sharepoint(image_content, uploaded_image.name, folder_url)
+            st.success("Image uploaded to SharePoint!")
+          
     if __name__ == "__main__":
         main()
