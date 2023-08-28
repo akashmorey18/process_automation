e4cd4140f403e1ae2d3d749ad63705b1782936c2
akashmorey18 <107257260+akashmorey18@users.noreply.github.com>
2023-08-28 17:11:38 +0530

    temp.py

--- temp.py
+++ temp.py
@@ -10,13 +10,9 @@ import streamlit as st
 from PIL import Image
 # import office365
 
-# from office365.runtime.auth.authentication_context import AuthenticationContext
-# from office365.sharepoint.client_context import ClientContext
-# from office365.sharepoint.files.file_creation_information import FileCreationInformation
-
-# from office365.sharepoint.client_context import ClientContext
-# from office365.runtime.auth.authentication_context import AuthenticationContext
-# from office365.sharepoint.files.file import File
+from office365.runtime.auth.authentication_context import AuthenticationContext
+from office365.sharepoint.client_context import ClientContext
+from office365.sharepoint.files.file_creation_information import FileCreationInformation
 
 # SharePoint authentication using client ID and client secret
 
