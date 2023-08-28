29d5c1215f737e6b4f4724a4ff970175663af2cf
akashmorey18 <107257260+akashmorey18@users.noreply.github.com>
2023-08-28 13:25:42 +0530

    temp.py

--- temp.py
+++ temp.py
@@ -6,9 +6,6 @@ import streamlit as st
 from PIL import Image
 from openpyxl import load_workbook
 
-import streamlit as st
-from PIL import Image
-
 from office365.runtime.auth.authentication_context import AuthenticationContext
 from office365.sharepoint.client_context import ClientContext
 from office365.sharepoint.file_creation_information import FileCreationInformation
